268b932c1d5c1671aeddd1bb36c4982adaffdb38
rogerphilippe@outlook.com <Um9nZXI>
2018-12-08 20:25:04 -0200

    APP Alcool x Gasolina

--- app/src/main/java/br/com/philippesis/alcoolougasolina/MainActivity.java
+++ app/src/main/java/br/com/philippesis/alcoolougasolina/MainActivity.java
@@ -2,6 +2,7 @@ package br.com.philippesis.alcoolougasolina;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -21,6 +22,28 @@ public class MainActivity extends AppCompatActivity {
         editGasolina = (EditText) findViewById(R.id.editGasolina);
         btnVerificar = (Button) findViewById(R.id.btnVerificar);
         txtExibicao = (TextView) findViewById(R.id.txtExibicao);
+
+        btnVerificar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String alcool = editAlcool.getText().toString();
+                String gasolina = editGasolina.getText().toString();
+
+                if ( alcool.isEmpty() || gasolina.isEmpty() ) {
+                    txtExibicao.setText("Um ou ambos os valores estão em branco!");
+                } else {
+
+                    if ( (Double.parseDouble(alcool) / Double.parseDouble(gasolina)) >= 0.7) {
+                        txtExibicao.setText("Compensa abastecer com gasolina! ;-)");
+                    } else {
+                        txtExibicao.setText("Compensa abastecer com álcool! ;-)");
+                    }
+
+                }
+
+            }
+        });
 
     }
 }
